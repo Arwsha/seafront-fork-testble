e11a9d46e087b530de4bf773b351ce3c3d5ef280
Arwsha <99677888+Arwsha@users.noreply.github.com>
2025-05-02 17:22:47 +0500

    Update SingleReview.vue

--- src/components/Reviews/SingleReview.vue
+++ src/components/Reviews/SingleReview.vue
@@ -3,17 +3,12 @@
         <div class="flex flex-row w-full gap-x-4">
             <img src="@/assets/img/ReviewsAvatars/test.jpeg" alt="User Avatar" class="w-10 h-10 rounded-full ring-2 ring-[#D4CDFF] ring-offset-4 ring-offset-[#09090D] shadow-custom">
             <div class="flex flex-row items-center">
-                <img src="@/assets/img/star.svg" class="w-[19.2px] h-[19.2px]">
-                <img src="@/assets/img/star.svg" class="w-[19.2px] h-[19.2px]">
-                <img src="@/assets/img/star.svg" class="w-[19.2px] h-[19.2px]">
-                <img src="@/assets/img/star.svg" class="w-[19.2px] h-[19.2px]">
-                <img src="@/assets/img/star.svg" class="w-[19.2px] h-[19.2px]">
+                <span class="text-[#D4CDFF] font-medium">{{ review.nickname }}</span>
             </div>
         </div>
         <p class="font-normal text-xl">{{ review.text }}</p>
 
-        <!-- Poloska images on the right -->
-        <div class="absolute right-0 top-0 h-full flex flex-row items-start	object-cover">
+        <div class="absolute right-0 top-0 h-full flex flex-row items-start object-cover">
             <img src="@/assets/img/poloska.png" class="h-full -mr-3">
             <img src="@/assets/img/poloska.png" class="h-full mr-5">
         </div>
@@ -25,6 +20,7 @@ const props = defineProps<{
   review: {
     text: string;
     avatar: string;
+    nickname: string;
   };
 }>();
 </script>
